829b2bd2f169394dd6eee6b1d36708c2692b5d72
Yorgen Galvis <yorgeneliecergr@ufps.edu.co>
2020-12-19 08:39:00 -0500

    Creacion del proyecto

---
+++ sorteo/src/test/java/com/examen/sorteo/SorteoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.sorteo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorteoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
